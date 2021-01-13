c2786cfa5f1e23614950eb55107b31ab7d781657
chicanei <chicanei@gmail.com>
2021-01-13 21:31:38 +0800

    Create char_assignment.c

---
+++ char_assignment.c
@@ -0,0 +1,19 @@
+//字元常數將字元的ASCII值和它所代表的字元連接起來
+//將字元常數指定給字元變數並印出來
+#include <stdio.h>
+int main(void){
+    char c;
+    c = 'H';
+    printf("%c", c);
+    c = 'E';
+    printf("%c", c);
+    c = 'L';
+    printf("%c", c);
+    c = 'L';
+    printf("%c", c);
+    c = 'O';
+    printf("%c", c);
+    c = '!';
+    printf("%c", c);
+    return 0;
+}
