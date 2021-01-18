d5f6dd522f1d2fe11e73ef2a7b65968960c64dff
chicanei <chicanei@gmail.com>
2021-01-19 01:06:29 +0800

    Create string-size.c

---
+++ string-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define STRINGLEN 80
+int main(void){
+  char s[STRINGLEN];
+  printf("%ld\n", sizeof(s));
+  return 0;
+}
+//印 80
+//片語：
+//#define STRINGLEN 80
+//char s[STRINGLEN];
